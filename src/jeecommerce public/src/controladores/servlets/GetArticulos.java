b92eeb7814e0b60ac9fb350c13f26a72b5f43b8c
Jesús González <dev@jesusd.net>
2016-04-23 20:16:49 +0200

    Creates configuration model class Creates initial get products servlet

---
+++ src/jeecommerce public/src/controladores/servlets/GetArticulos.java
@@ -0,0 +1,89 @@
+package controladores.servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import modelos.MArticulos;
+import modelos.MConfiguracion;
+
+/**
+ * Servlet implementation class GetArticulos
+ */
+@WebServlet("/articulos/get")
+public class GetArticulos extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException
+	{
+		HttpSession sesion = request.getSession();
+		Connection conexion = (Connection) sesion.getAttribute("conexion");
+		
+		if (conexion == null)
+		{
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+		
+		// Obtener numer de elementos por página de la base de datos
+		int numElementosPorPagina = -1;
+		int pagina = -1;
+		
+		try
+		{
+			MConfiguracion mdlConfiguracion = new MConfiguracion(conexion);
+			mdlConfiguracion.getParametro("NumElementosPorPagina");
+			
+			if (mdlConfiguracion.getProximoParametro())
+			{
+				 numElementosPorPagina = Integer.parseInt(mdlConfiguracion.valor);
+			}
+			else
+			{
+				response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+				return;
+			}
+			
+			pagina = Integer.parseInt(request.getParameter("pagina"));
+		}
+		 catch (NumberFormatException x)
+		{
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			return; 
+		}
+		
+		MArticulos mdlArticulos = new MArticulos(conexion);
+		mdlArticulos.getLatestArticulos(numElementosPorPagina, (pagina * numElementosPorPagina));
+		
+		JsonArray jsonArticulos = new JsonArray();
+		JsonObject jsonArticulo;
+		
+		while (mdlArticulos.getProximoArticulo())
+		{
+			jsonArticulo = new JsonObject();
+			
+			
+			jsonArticulo.addProperty("artid", mdlArticulos.artid);
+			jsonArticulo.addProperty("nombre", mdlArticulos.nombre);
+			jsonArticulo.addProperty("precio", mdlArticulos.precio);
+			
+			jsonArticulos.add(jsonArticulo);
+		}
+		
+		response.setContentType("application/json");
+		response.setCharacterEncoding("utf-8");
+		response.getWriter().write(jsonArticulos.toString());
+		return;
+	}
+
+}
